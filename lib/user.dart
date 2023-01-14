b591d1f9e850360231317a90ad6635c0ad80e669
Aashutosh10 <aashutoshneupane12@gmail.com>
2023-01-14 19:53:05 +0545

    project progress

--- lib/user.dart
+++ lib/user.dart
@@ -1,121 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:prominor/dataentry.dart';
-import 'package:prominor/wallet.dart';
+import 'package:prominor/sidebar.dart';
+class homepage extends StatefulWidget {
+  const homepage({Key? key}) : super(key: key);
 
-
-class HomeScreen extends StatefulWidget {
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<homepage> createState() => _homepageState();
 }
 
-  class _HomeScreenState extends State<HomeScreen> {
-
-    var children =
-    [
-      DataEntryPage(),
-      WalletPage()
-    ];
-    int _currentindex =0;
-    void onTappedBar(int index) {
-      setState(() {
-        _currentindex = index;
-      });
-
-      switch (index) {
-        case 0:
-        // do something
-          break;
-        case 1:
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => DataEntryPage()),
-          );
-          break;
-        case 2:
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => WalletPage()),
-          );
-          break;
-      }
-    }
-
-
+class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      backgroundColor: Colors.grey[300],
+      drawer: sideNav(),
+
       appBar: AppBar(
-        title: Text('Home Page'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.person_pin),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text('User'),
-              accountEmail: Text('user@email.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                  'https://pbs.twimg.com/profile_images/1307731275781177344/k5aVBG5Z_400x400.jpg',
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.exit_to_app),
-              title: Text('Logout'),
-              onTap: () {},
-            ),
-          ],
-        ),
+        backgroundColor: Colors.green,
+        title: Text('HomePage'),
+
       ),
       body: Center(
-        child: Text('User Old Items Are Displayed Here'),
+        child: Text('Users previous items are shown here'),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTappedBar,
-        currentIndex: _currentindex,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.white,
 
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'Data',
-            backgroundColor: Colors.white,
-
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Setting',
-            backgroundColor: Colors.white,
-          ),
-
-        ],
-      ),
     );
   }
 }
-
-
-
-
